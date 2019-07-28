003134de6ad67946904de73a9cbb905fe36aaa8b
Mariusz Złotucha <mariusz.zlotucha@gmail.com>
2019-07-28 11:12:43 +0200

    chapter 1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { readFileSync } from 'fs';
+import { readFileSync, fstat } from 'fs';
 import http from 'http';
+import fs from 'fs';
 import * as process from 'process';
 import * as WebSocket from 'ws';
 import {
@@ -19,3 +20,26 @@ const FILE_EXTENSION_TO_CONTENT_TYPE: FileExtensionToContentTypeMap = {
   ico: 'image/x-icon',
   js: 'text/javascript',
 };
+
+const server = http.createServer((request, response) => {
+  try {
+    const url = request.url && request.url !== '/' ? request.url  : 'index.html';
+
+    const urlParts = url.split('.');
+    const fileExtension = urlParts[urlParts.length - 1]  as StaticFileExtension;
+    const contentType = FILE_EXTENSION_TO_CONTENT_TYPE[fileExtension];
+    response.writeHead(200, { 'Content-Type': contentType });
+
+
+    const file = fs.readFileSync(`${process.cwd()}/public/${url}`);
+
+    response.end(file);
+  } catch (err) {
+    console.log(err);
+    response.end(err.toString())
+  }
+})
+
+server.listen(PORT, () => {
+  console.log('ruszył');
+});
